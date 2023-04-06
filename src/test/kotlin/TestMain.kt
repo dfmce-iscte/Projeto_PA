6c196c6d0b6d81a893a76c1ddd9b4ddb082ea6da
dfmce-iscte <dfmce@iscte-iul.pt>
2023-04-06 15:48:36 +0100

    Tests para o ponto 2 dos Visitantes

--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -1,30 +1,45 @@
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TestMain {
 
+    val rootObject = ObjectJSON()
+    val n = JSONNumber(rootObject,1,"number")
+    val array = ArrayJSON("array",rootObject)
+    val b1 = JSONBoolean(array,true)
+    val b2 = JSONBoolean(array,false)
+    val arr2 = ArrayJSON("arr2",rootObject)
+    val arrayObject = ObjectJSON("arrayObject",arr2)
+    val n1 = JSONNumber(arrayObject,101101,"numero")
+    val no1 = JSONString(arrayObject,"Dave Farley","nome")
+    val i1 = JSONBoolean(arrayObject,true,"internacional")
+    val arrayObject2 = ObjectJSON("arrayObject2",arr2)
+    val n2 = JSONNumber(arrayObject2,101102.1,"numero")
+    val no2 = JSONString(arrayObject2,"Martin Fowler","nome")
+    val i2 = JSONBoolean(arrayObject2,true,"internacional")
+    val nul = JSONNull(rootObject,"null")
+
     @Test
     fun `test object creation` () {
-        val rootObject = ObjectJSON()
-
-        //faz-se JSONNumber(rootObject,1) ou rootObject.addElement(JSONNumber(1))???
-        JSONNumber(rootObject,1,"number")
-
-        val array = ArrayJSON("array",rootObject)
-        JSONBoolean(array,true)
-        JSONBoolean(array,false)
-
-        val arr2 = ArrayJSON("arr2",rootObject)
-        val arrayObject = ObjectJSON("arrayObject",arr2)
-        JSONNumber(arrayObject,101101,"numero")
-        JSONString(arrayObject,"Dave Farley","nome")
-        JSONBoolean(arrayObject,true,"internacional")
-
-        val arrayObject2 = ObjectJSON("arrayObject2",arr2)
-        JSONNumber(arrayObject2,101102,"numero")
-        JSONString(arrayObject2,"Martin Fowler","nome")
-        JSONBoolean(arrayObject2,true,"internacional")
-
-        JSONNull(rootObject,"null")
         println(rootObject)
+    }
+
+
+    @Test
+    fun testCheckStructure() {
+        val cs = CheckStructure("nome",String::class)
+        rootObject.accept(cs)
+        assertEquals(expected = true, cs.valid)
+
+        val cs_numero = CheckStructure("numero",Int::class)
+        rootObject.accept(cs_numero)
+        assertEquals(expected = false, cs_numero.valid)
+    }
+
+    @Test
+    fun testCheckArrayStructure() {
+        val cas = CheckArrayStructure("arr2",arrayObject)
+        rootObject.accept(cas)
+        assertEquals(expected = true, cas.valid)
     }
 }
